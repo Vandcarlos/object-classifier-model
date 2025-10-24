1556e92c4b375ab73325fe9b8e75e95df8dd5912
Vand <vandcarlosjunior@gmail.com>
2025-10-24 00:20:38 -0300

    Removed unecessary logs

--- infra/stack/oidc_github/main.tf
+++ infra/stack/oidc_github/main.tf
@@ -5,14 +5,6 @@ provider "aws" {
 # Debug: quem sou eu, e qual role a esteira assumiu?
 data "aws_caller_identity" "me" {}
 
-data "aws_iam_role" "github_deploy" {
-  name = "github-ml-artifacts-deploy"
-}
-
 output "whoami_account" {
   value = data.aws_caller_identity.me.account_id
-}
-
-output "github_role_arn" {
-  value = data.aws_iam_role.github_deploy.arn
 }
