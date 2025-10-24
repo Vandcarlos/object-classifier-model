d80fdab7c78f1a8000d4437c2feffbf4c2d6c08e
Vand <vandcarlosjunior@gmail.com>
2025-10-23 23:36:42 -0300

    Fixed bootstrap policy

---
+++ infra/bootstrap_state/s3_policy.tf
@@ -0,0 +1,42 @@
+data "aws_iam_policy_document" "tfstate_bucket_policy" {
+  statement {
+    sid     = "ListStatePrefix"
+    effect  = "Allow"
+    actions = ["s3:ListBucket"]
+    resources = ["arn:aws:s3:::${var.state_bucket_name}"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_role.github_actions.arn]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "s3:prefix"
+      values   = ["${var.state_bucket_prefix}/*"]
+    }
+  }
+
+  statement {
+    sid     = "CRUDStateObjects"
+    effect  = "Allow"
+    actions = ["s3:GetObject","s3:PutObject","s3:DeleteObject","s3:AbortMultipartUpload"]
+    resources = ["arn:aws:s3:::${var.state_bucket_name}/${var.state_bucket_prefix}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_role.github_actions.arn]
+    }
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["true"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "tfstate" {
+  bucket = var.state_bucket_name
+  policy = data.aws_iam_policy_document.tfstate_bucket_policy.json
+}
